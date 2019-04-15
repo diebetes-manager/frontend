e2910c76f7195f3fb455077de5ccf77119d069e7
josephriosio <blankworldwide@gmail.com>
2019-04-15 16:09:47 -0700

    added dummy data for profile

--- diabetes-manager/src/components/dashboard/Profile.js
+++ diabetes-manager/src/components/dashboard/Profile.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-const Profile = props => {
-  return <h1>profile</h1>;
-};
-export default Profile;
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  input: {
+    display: "none"
+  }
+});
+
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "joseph rios",
+        age: 35,
+        height: "5 ft 9in",
+        weight: "140 lbs"
+      }
+    };
+  }
+  render() {
+    const { classes } = this.props;
+    const { name, age, height, weight } = this.state.user;
+    return (
+      <div>
+        <h1>Profile</h1>
+        <p>name: {name}</p>
+        <p>age: {age}</p>
+        <p>height: {height}</p>
+        <p>weight: {weight}</p>
+        <Button variant="contained" color="primary" className={classes.button}>
+          Edit
+        </Button>
+      </div>
+    );
+  }
+}
+export default withStyles(styles)(Profile);
